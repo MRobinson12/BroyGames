e440dcc1bb2844400a31eab5e25ddaf1ec155c2f
Morgan <43744780+MRobinson12@users.noreply.github.com>
2024-07-18 22:21:31 +0100

    Added Main Menu and Credits Framework

---
+++ mainmenu.gd
@@ -0,0 +1,10 @@
+extends Node
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/test_scene.tscn")
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
