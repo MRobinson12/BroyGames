096783b5c7fcb0a5e26ab9ccadab5915b59289cc
Josh Hull <jjh43@student.le.ac.uk>
2024-07-23 21:39:51 +0100

    Scene changes

--- Scripts/player_light.gd
+++ Scripts/player_light.gd
@@ -10,7 +10,8 @@ var camera : Camera2D
 func _ready():
 	target_marker = get_parent().find_child("Player").find_child("LightPosition")
 	shadow_shader = get_parent().get_node("ShaderLayer/PlayerVignette")
-	camera = get_parent().get_node("Camera2D")
+	camera = %Camera2D
+	global_transform.origin = target_marker.global_transform.origin
 
 func _process(delta: float):
 	player_position = get_global_transform_with_canvas().get_origin() / Vector2(get_viewport().size)
