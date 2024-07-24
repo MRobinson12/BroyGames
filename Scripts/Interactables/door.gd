9bb960e48c1d1148a7230f0ec5bf867055c34030
Josh Hull <jjh43@student.le.ac.uk>
2024-07-24 19:38:22 +0100

    Open doors

--- Scripts/Interactables/door.gd
+++ Scripts/Interactables/door.gd
@@ -8,8 +8,11 @@ func _ready():
 	collision_shape = $CollisionShape2D
 	super._ready()
 
+func _process(_delta):
+	if Input.is_action_just_pressed("interact"):
+		check_player_inventory()
+	
 # check the players inventory for a key with the ID 
-# HEY JOSSSHHHHH
 func check_player_inventory():
 	for item in GlobalData.player_inventory.contents:
 		if item is KeyItem:
