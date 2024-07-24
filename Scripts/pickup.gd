fc65da7d5c1d07dc26aedf5a15834387d16806a8
Josh Hull <jjh43@student.le.ac.uk>
2024-07-24 13:39:22 +0100

    Optional pickup highlight shader

--- Scripts/pickup.gd
+++ Scripts/pickup.gd
@@ -2,3 +2,8 @@ extends Area2D
 class_name Pickup
 
 @export var item : Item
+@export var shine : bool
+
+func _ready() -> void:
+	$Sprite2D.texture = item.icon
+	$Sprite2D.material.set_shader_parameter("enabled", int(shine))
