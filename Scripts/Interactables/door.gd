82c95f0568ece06acd61aa38f2d8cb3418a2069a
kerdis01 <kyle.erdis@gmail.com>
2024-07-25 08:11:44 +0100

    Updated door and key interaction
    Door can now be tried without a key. Once key is acquired, player has to hit e on the door.

--- Scripts/Interactables/door.gd
+++ Scripts/Interactables/door.gd
@@ -3,22 +3,32 @@ class_name Door
 
 @export var required_key_id: String
 
+var player_in_range: bool = false
+
 func _ready():
 	sprite = $DoorSprite
 	collision_shape = $CollisionShape2D
 	super._ready()
+	$InteractionArea.connect("body_entered", _on_body_entered)
+	$InteractionArea.connect("body_exited", _on_body_exited)
 
 func _process(_delta):
-	if Input.is_action_just_pressed("interact"):
+	if Input.is_action_just_pressed("interact") and player_in_range:
 		check_player_inventory()
-	
-# check the players inventory for a key with the ID 
+
+func _on_body_entered(_body):
+	player_in_range = true
+
+func _on_body_exited(_body):
+	player_in_range = false
+
+# Check the player's inventory for a key with the ID 
 func check_player_inventory():
 	for i in range(GlobalData.player_inventory.contents.size()):
 		var item = GlobalData.player_inventory.contents[i]
-		if item is KeyItem:
-			if item.key_id == required_key_id:
-				open()
-				GlobalData.player_inventory.remove_item(i)
-				return
-			
+		if item is KeyItem and item.key_id == required_key_id:
+			open()
+			GlobalData.player_inventory.remove_item(i)
+			# Add door open sound here
+			return
+	# Add door rattle sound here
