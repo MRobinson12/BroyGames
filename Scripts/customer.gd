2a76e9292fff9c7bb12bdafb20c45cb7373a6838
Josh Hull <jjh43@student.le.ac.uk>
2024-07-27 20:18:12 +0100

    Shop updates

--- Scripts/customer.gd
+++ Scripts/customer.gd
@@ -11,7 +11,15 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-
 	velocity.x = direction * speed
+	
+	if velocity.x == 0:
+		$AnimatedSprite2D.play("idle")
+	elif velocity.x > 0:
+		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = false
+	elif velocity.x < 0:
+		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = true
 
 	move_and_slide()
--- Scripts/shop_test.gd
+++ Scripts/shop_test.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
+signal crafted
+
 var customer_scene = preload("res://Scenes/customer.tscn")
 @onready var dialogue_box = $DialogueBox
+@onready var crafting_menu = %CraftingMenu
+var crafted_potion : Item
 
 @export var available_potions  : Array[CraftRecipe]
 var customers : Array[Customer]
@@ -9,6 +13,7 @@ var customer_amount = 5
 var current_customer : Customer
 
 func _ready() -> void:
+	GlobalData.potion_crafted.connect(potion_crafted)
 	spawn_customers()
 	next_customer()
 	pass
@@ -19,16 +24,23 @@ func spawn_customers():
 		new_customer.wanted_potion = available_potions[randi_range(0, available_potions.size()-1)]
 		customers.append(new_customer)
 		$CustomerSpawn.add_child(new_customer)
-	
+
 
 func next_customer():
-	current_customer = customers[0]
-	customers.remove_at(0)
-	current_customer.direction = -1
+	if current_customer:
+		current_customer.direction = 1
+	if customers:
+		current_customer = customers[0]
+		customers.remove_at(0)
+		current_customer.direction = -1
+		
+func potion_crafted():
+	pass
 
 func _on_dialogue_signal(value):
 	match(value):
-		'next_customer': current_customer.direction = 1
+		'next_customer': next_customer()
+		'craft' : crafting_menu.show()
 
 func _on_dialogue_area_body_entered(body: Node2D) -> void:
 	if body is Customer:
