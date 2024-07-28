8faea9250174d7f7051201d194f8cc9f52f39675
Morgan <43744780+MRobinson12@users.noreply.github.com>
2024-07-28 21:50:47 +0100

    Fixed Pause Menu Bug + Redid some Menus

--- Scenes/UI/pause_menu.gd
+++ Scenes/UI/pause_menu.gd
@@ -7,4 +7,5 @@ func _on_resume_pressed() -> void:
 
 
 func _on_quit_pressed() -> void:
+	Engine.time_scale = 1
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
--- Scripts/Scene/tutorial.gd
+++ Scripts/Scene/tutorial.gd
@@ -5,6 +5,7 @@ var paused = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	paused = false
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
