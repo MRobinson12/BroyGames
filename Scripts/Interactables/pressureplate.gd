6a25994cd6dfcf7f6cd6cd2bb71dc87291f3f2f8
kerdis01 <kyle.erdis@gmail.com>
2024-07-29 23:36:23 +0100

    Allowing players to stand on pressure plates WIP
    doesnt work lol

--- Scripts/Interactables/pressureplate.gd
+++ Scripts/Interactables/pressureplate.gd
@@ -6,19 +6,28 @@ signal toggled(is_on)
 @onready var sprite = $PressureplateSprite
 
 func _ready():
-	connect("body_entered", _on_body_entered)
-	connect("body_exited", _on_body_exited)
+	connect("body_entered", Callable(self, "_on_body_entered"))
+	connect("body_exited", Callable(self, "_on_body_exited"))
 	update_appearance()
 
 func _on_body_entered(body):
-	if body.is_in_group("WoodenCrate") and not is_on:
+	if (body.is_in_group("WoodenCrate") or body.is_in_group("Player")) and not is_on:
 		$PressurePlatePressed.play()
 		toggle()
 
 func _on_body_exited(body):
-	if body.is_in_group("WoodenCrate") and is_on:
-		$PressurePlateReleased.play()
-		toggle()
+	if (body.is_in_group("WoodenCrate") or body.is_in_group("Player")) and is_on:
+		# Check if there are still valid bodies on the pressure plate
+		var bodies = get_overlapping_bodies()
+		var still_pressed = false
+		for overlapping_body in bodies:
+			if overlapping_body.is_in_group("WoodenCrate") or overlapping_body.is_in_group("Player"):
+				still_pressed = true
+				break
+		
+		if not still_pressed:
+			$PressurePlateReleased.play()
+			toggle()
 
 func toggle():
 	is_on = !is_on
