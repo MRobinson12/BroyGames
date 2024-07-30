4c04883842913b0293456b70daa308ddf107a033
Josh Hull <jjh43@student.le.ac.uk>
2024-07-31 00:38:22 +0100

    Light stoof

--- Scripts/player_light.gd
+++ Scripts/player_light.gd
@@ -9,9 +9,20 @@ var camera : Camera2D
 
 @export var lights : Array[Node2D]
 var light_positions : Array[Vector2]
+var timer
+var shrinking : bool = false
+var shrink_rate : float = 0.9995
+var current_scale : float = 0.16
 
 func _ready():
 	light_positions.resize(50)
+	
+	timer = Timer.new()
+	timer.wait_time = 2.0
+	timer.one_shot = true
+	timer.autostart = true
+	timer.connect("timeout", start_shrink)
+	add_child(timer)
 	
 	target_marker = get_parent().find_child("Player").find_child("LightPosition")
 	shadow_shader = get_parent().get_node("ShaderLayer/PlayerVignette")
@@ -28,6 +39,12 @@ func _process(delta: float):
 			light_positions[i] = lights[i].get_global_transform_with_canvas().get_origin() / Vector2(get_viewport().size)
 		shadow_shader.material.set_shader_parameter("lights", light_positions)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func start_shrink():
+	#shrinking = true
+	pass
+
 func _physics_process(delta: float):
+	if shrinking:
+		current_scale = current_scale * shrink_rate
+		shadow_shader.material.set_shader_parameter("player_scale", current_scale)
 	global_transform.origin = lerp(global_transform.origin, target_marker.global_transform.origin, delta * lerp_speed)
