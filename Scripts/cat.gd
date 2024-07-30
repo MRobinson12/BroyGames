822417d44b0c86bb26179fd6b9b6f1e346459eec
Josh Hull <jjh43@student.le.ac.uk>
2024-07-30 14:08:43 +0100

    Speak to cat

--- Scripts/cat.gd
+++ Scripts/cat.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+@onready var dialogue_box = %DialogueBox
+@export var dialogue_id : String
+var player_in_range : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +12,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if player_in_range:
+		if Input.is_action_just_pressed("interact"):
+			dialogue_box.start(dialogue_id)
+
+
+func _on_body_entered(body):
+	if body is CharacterBody2D:
+		player_in_range = true
+		$InputPrompt.show()
+
+func _on_body_exited(body):
+	if body is CharacterBody2D:
+		player_in_range = false
+		$InputPrompt.hide()
