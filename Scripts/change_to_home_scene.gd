db7be80986d29630d02a3dce3a9c5b1281f0ede1
kerdis01 <kyle.erdis@gmail.com>
2024-07-30 21:45:50 +0100

    Cave Design Handover
    bean

--- Scripts/change_to_home_scene.gd
+++ Scripts/change_to_home_scene.gd
@@ -5,9 +5,10 @@ var target_scene = "res://Scenes/home_scene.tscn"
 func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
 
-func _on_body_entered(body):
+func _on_body_entered(body):   
 	if body is CharacterBody2D:
 		if target_scene:
+			GlobalData.move_inventory()
 			get_tree().change_scene_to_file(target_scene)
 		else:
 			print("Target scene is not set!")
