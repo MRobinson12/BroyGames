78bc7b45c1ac4f962cfeab888d82c77f6bf1f6b4
Josh Hull <jjh43@student.le.ac.uk>
2024-07-31 02:37:04 +0100

    Update change_to_home_scene.gd

--- Scripts/change_to_home_scene.gd
+++ Scripts/change_to_home_scene.gd
@@ -9,6 +9,9 @@ func _on_body_entered(body):
 	if body is CharacterBody2D:
 		if target_scene:
 			GlobalData.move_inventory()
-			get_tree().change_scene_to_file(target_scene)
+			call_deferred("change_scene")
 		else:
 			print("Target scene is not set!")
+
+func change_scene():
+	get_tree().change_scene_to_file(target_scene)
